76327b0b2547cf1505df642c22366a8c2433ec3c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-03-29 07:18:19 +0530

    adding const for better performance

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,7 +3,6 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
   final Function deleteTx;
 
@@ -12,49 +11,56 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final delIcon = (index) => IconButton(
+          icon: const Icon(Icons.delete),
+          onPressed: () {
+            return deleteTx(transactions[index].id);
+          },
+          color: Theme.of(context).colorScheme.error,
+        );
+    final FlatIcon = (index) => ElevatedButton.icon(
+          icon: const Icon(Icons.delete),
+          label: const Text("Delete"),
+          onPressed: () {
+            return deleteTx(transactions[index].id);
+          },
+          style: TextButton.styleFrom(
+              foregroundColor: Theme.of(context).colorScheme.error,
+              backgroundColor: Colors.white),
+        );
 
-    final delIcon = (index) => IconButton(icon: Icon(Icons.delete), onPressed: () { return deleteTx(transactions[index].id);}, color: Theme.of(context).colorScheme.error,);
-    final FlatIcon = (index) => ElevatedButton.icon(icon: Icon(Icons.delete), label: Text("Delete"), onPressed: () { return deleteTx(transactions[index].id);}, style: TextButton.styleFrom(
-
-        foregroundColor: Theme.of(context).colorScheme.error,
-        backgroundColor: Colors.white
-      ),);
-
-
-    return  Container(
-      padding: EdgeInsets.all(10),
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          var tx = transactions[index];
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            var tx = transactions[index];
             return Card(
               margin: EdgeInsets.symmetric(vertical: 10),
               shape: RoundedRectangleBorder(
                 side: BorderSide(color: Colors.white70, width: 1),
                 borderRadius: BorderRadius.circular(10),
-  ),
+              ),
               child: ListTile(
                 leading: CircleAvatar(
-                  radius: 30, 
+                  radius: 30,
                   child: Padding(
-                    padding:EdgeInsets.all(6), 
-                    child: FittedBox(
-                      child: Text('\$${transactions[index].amount}')
-                    )
-                  ),
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                          child: Text('\$${transactions[index].amount}'))),
                 ),
-                title: Text(
-                  transactions[index].title
-                ),
+                title: Text(transactions[index].title),
                 subtitle: Text(
-                        DateFormat.yMMMMd('en_US').format(tx.date),
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                trailing: MediaQuery.of(context).size.width > 360 ? FlatIcon(index) : delIcon(index),
+                  DateFormat.yMMMMd('en_US').format(tx.date),
+                  style: const TextStyle(color: Colors.grey),
+                ),
+                trailing: MediaQuery.of(context).size.width > 360
+                    ? FlatIcon(index)
+                    : delIcon(index),
               ),
             );
           },
-        itemCount: transactions.length,
-      ));
+          itemCount: transactions.length,
+        ));
   }
 }
 
